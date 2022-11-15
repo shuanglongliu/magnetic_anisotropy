275686dd5b6ca5e04849f57bee31c2fa0cd84bb1
shuanglongliu <shlliutlb@gmail.com>
2022-11-15 08:15:48 -0800

    Add get_all_energies in main.py.
    Add exchange coupling in main.py.

--- local/main.py
+++ local/main.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from common import *
 
-e_ref = -2277.46276603
+e_ref = -686.37415457
 
 def check_and_resubmit():
 
@@ -24,11 +24,11 @@ def find_global_directions():
 
 def do():
 
-    myjob = vasp_jobs_ncl(n_site=3, magnetic_moments=[3, 4, 4], n_theta=1801, n_phi=3600)
+    myjob = vasp_jobs_ncl(n_site=4, magnetic_moments=[2, 2, 2, 3], n_theta=1801, n_phi=3600)
     myjob.setup_local_ref_frames(sites=[0, 1, 2], from_file=True)
 
-    myjob.add_configurations([[[  0,0], [  0,0], [  0,0]]], local_ref_frame=True)
-    myjob.add_configurations([[[180,0], [180,0], [180,0]]], local_ref_frame=False)
+    #myjob.add_configurations([[[  0,0], [  0,0], [  0,0]]], local_ref_frame=True)
+    #myjob.add_configurations([[[180,0], [180,0], [180,0]]], local_ref_frame=False)
 
     #direction_emin = (42.9, 102.4)
     #myjob.setup_local_ref_frames(sites=[0, 1, 2], z_directions=[direction_emin, direction_emin], from_file=False)
@@ -40,13 +40,36 @@ def do():
     #myjob.add_thetas_colinear_spin(  phi=0, theta_min=180, theta_max=174, ntheta=4, local_ref_frame=True)
     #myjob.add_thetas_colinear_spin(phi=270, theta_min=174, theta_max=180, ntheta=4, local_ref_frame=True)
     #myjob.add_thetas_colinear_spin( phi=90, theta_min=180, theta_max=174, ntheta=4, local_ref_frame=True)
+
     #myjob.add_thetas_colinear_spin(  phi=0, theta_min= 0, theta_max=180, ntheta= 7, local_ref_frame=True)
     #myjob.add_thetas_colinear_spin(phi=180, theta_min= 0, theta_max=180, ntheta= 7, local_ref_frame=True)
     #myjob.add_thetas_colinear_spin( phi=90, theta_min= 0, theta_max=180, ntheta= 7, local_ref_frame=True)
     #myjob.add_thetas_colinear_spin(phi=270, theta_min= 0, theta_max=180, ntheta= 7, local_ref_frame=True)
     #myjob.add_phis_colinear_spin( theta=90,   phi_min= 0,   phi_max=330,   nphi=12, local_ref_frame=True)
 
-    #myjob.set_direction_for_one_site(i_site=1, direction=[0, 0], local_ref_frame=True)
+    myjob.add_configurations([[[90, 0], [180, 0], [180, 0], [ 90, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[90,90], [180, 0], [180, 0], [ 90, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[ 0, 0], [180, 0], [180, 0], [ 90, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[90, 0], [180, 0], [180, 0], [ 90,90]]], local_ref_frame=True)
+    myjob.add_configurations([[[90,90], [180, 0], [180, 0], [ 90,90]]], local_ref_frame=True)
+    myjob.add_configurations([[[ 0, 0], [180, 0], [180, 0], [ 90,90]]], local_ref_frame=True)
+    myjob.add_configurations([[[90, 0], [180, 0], [180, 0], [180, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[90,90], [180, 0], [180, 0], [180, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[ 0, 0], [180, 0], [180, 0], [180, 0]]], local_ref_frame=True)
+    myjob.flip_direction_for_one_site(i_site=0)                     
+                                                                    
+    myjob.add_configurations([[[90, 0], [180, 0], [180, 0], [ 90, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[90,90], [180, 0], [180, 0], [ 90, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[ 0, 0], [180, 0], [180, 0], [ 90, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[90, 0], [180, 0], [180, 0], [ 90,90]]], local_ref_frame=True)
+    myjob.add_configurations([[[90,90], [180, 0], [180, 0], [ 90,90]]], local_ref_frame=True)
+    myjob.add_configurations([[[ 0, 0], [180, 0], [180, 0], [ 90,90]]], local_ref_frame=True)
+    myjob.add_configurations([[[90, 0], [180, 0], [180, 0], [180, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[90,90], [180, 0], [180, 0], [180, 0]]], local_ref_frame=True)
+    myjob.add_configurations([[[ 0, 0], [180, 0], [180, 0], [180, 0]]], local_ref_frame=True)
+
+    #myjob.set_direction_for_one_site(i_site=0, direction=[180, 0], local_ref_frame=True)
+    #myjob.set_direction_for_one_site(i_site=1, direction=[180, 0], local_ref_frame=True)
     #myjob.set_direction_for_one_site(i_site=2, direction=[180, 0], local_ref_frame=True)
     #myjob.flip_direction_for_one_site(i_site=2)
 
@@ -58,11 +81,14 @@ def do():
     #myjob.print_dirs_and_configs(local_ref_frame=False)
     #myjob.setup_jobs(submit=False)
     #myjob.setup_jobs(submit=True)
-    #restart(myjob, test=True, max_angle=45, max_energy=0.1, from_neighbor=True, file_name="CHGCAR", copy_it=False)
-    #restart(myjob, test=False, max_angle=45, max_energy=0.1, from_neighbor=True, file_name="CHGCAR", copy_it=False)
+    #restart(myjob, test=True, max_angle=45, max_energy=0.1, from_neighbor=True, file_name="WAVECAR", copy_it=False)
+    #restart(myjob, test=False, max_angle=45, max_energy=0.1, from_neighbor=True, file_name="WAVECAR", copy_it=False)
+    #myjob.check_convergences(restart=False, de0=1.e-8)
+    #myjob.check_convergences(restart=True, de0=1.e-8)
     #myjob.check_local_magmoms_all_configurations()
     #myjob.get_occmat_eigenvectors_all_configurations()
-    myjob.get_energies(max_energy=0.1, de0=1.e-5)
+    myjob.get_energies(max_energy=0.1, de0=1.e-8)
+    #get_all_energies(myjob, max_energy=0.1, de0=1.e-8)
 
 if __name__ == "__main__":
 
