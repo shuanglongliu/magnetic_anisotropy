7492bbd83db4983509b72342bf9ce8a320b3e44a
shuanglongliu <shlliutlb@gmail.com>
2022-12-21 07:36:32 -0800

    Turn direction_emin, e_ref, max_energy, and de0 as arguments of do().

--- global/main.py
+++ global/main.py
@@ -10,7 +10,7 @@ def find_global_directions():
     myjob.print_dirs_and_configs(local_ref_frame=False)
     #print("Opposite direction: {:6.1f} {:6.1f}".format(*opposite_direction))
 
-def do(e_ref = 0.0, direction_emin = (0.0, 0.0)):
+def do(direction_emin = (0.0, 0.0), e_ref = 0.0, max_energy = np.inf, de0 = 1.e-8):
 
     myjob = vasp_jobs_ncl(n_theta=1801, n_phi=3600)
     #myjob.setup_local_ref_frames(z_direction=direction_emin, from_file=True)
@@ -46,19 +46,19 @@ def do(e_ref = 0.0, direction_emin = (0.0, 0.0)):
     #myjob.print_dirs_and_configs(local_ref_frame=False)
     #myjob.setup_jobs(submit=False)
     #myjob.setup_jobs(submit=True)
-    #myjob.check_convergences(restart=False, de0=1.e-7)
-    #myjob.check_convergences(restart=True, de0=1.e-7)
+    #myjob.check_convergences(restart=False, de0=de0)
+    #myjob.check_convergences(restart=True, de0=de0)
     #myjob.check_local_magmoms_all_configurations()
-    #restart(myjob, test=True, max_energy=0.01)
-    #restart(myjob, test=False, max_energy=0.01)
-    myjob.get_energies(max_energy=np.inf, de0=1.e-7)
-    #get_all_energies(myjob, max_energy=0.01, de0=1.e-7)
+    #restart(myjob, test=True, max_energy=max_energy)
+    #restart(myjob, test=False, max_energy=max_energy)
+    myjob.get_energies(max_energy=max_energy, de0=de0)
+    #get_all_energies(myjob, max_energy=max_energy, de0=de0)
 
 if __name__ == "__main__":
 
     #find_global_directions()
 
-    do(e_ref = -1834.36333292, direction_emin = (98.282, 330.142))
+    do(direction_emin = (98.282, 330.142), e_ref = -1834.36333292, max_energy = np.inf, de0 = 1.e-8)
 
     pass
 
