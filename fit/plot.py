45d5979ee056325d85f3d00d59694f6ba54b961c
shuanglongliu <shlliutlb@gmail.com>
2023-09-15 09:48:36 -0400

    Updated plot.py

--- fit/plot.py
+++ fit/plot.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0):
-    # factor: scale factor for the energy. useful when multiple equivalent spins are rotated togethe.
+def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0, wavenumber=True):
+    # factor: scale factor for the energy. useful when multiple equivalent spins are rotated together.
 
     columns = np.array(columns, dtype=int) - 1
 
@@ -18,7 +18,6 @@ def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0):
         return
     
     energies = np.loadtxt(fin)
-    #print(energies[-5:]); exit()
     
     
     
@@ -29,7 +28,6 @@ def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0):
     column3 = columns[2]
     
     emin = np.nanmin(energies[:, column3])
-    #print("{:15.8f}".format(emin))
     
     nconfig = energies.shape[0]
     
@@ -37,20 +35,22 @@ def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0):
         if energies[i, column3] == 0.0:
             energies[i, column3] = np.nan
         else:
-            energies[i, column3] = 1000*factor*(energies[i, column3] - emin)
+            if wavenumber:
+                energies[i, column3] = 8.06554393738*1000*factor*(energies[i, column3] - emin)
+            else:
+                energies[i, column3] =               1000*factor*(energies[i, column3] - emin)
     
     emin = np.nanmin(energies[:, column3])
     emax = np.nanmax(energies[:, column3])
-    #print("{:10.5f} {:10.5f}".format(emin, emax)); exit()
 
     energies[:, column1] = np.deg2rad(energies[:, column1])
     energies[:, column2] = np.deg2rad(energies[:, column2])
-    
+
     
     
     ### Plot
     
-    fig = plt.figure(figsize=(5.5, 4.5))
+    fig = plt.figure(figsize=(4.2, 3.2))
     ax = fig.add_subplot(projection="polar")
     
     mpl.rcParams["font.family"] = "Times New Roman"
@@ -60,16 +60,15 @@ def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0):
     
     ax.set_xlim([0, 2*np.pi])
     ax.set_ylim([0, np.pi])
-    #ax.set_ylim([0, np.pi/180*6.5])
 
-    ax.set_yticks(np.linspace(0, np.pi, 7, endpoint=True), labels=[u"0\u00b0", u"30\u00b0", u"60\u00b0", u"90\u00b0", u"120\u00b0", u"150\u00b0", u"180\u00b0"])
-    #ax.set_yticks(np.linspace(0, np.pi/180*6.0, 4, endpoint=True), labels=[u"0\u00b0", u"2\u00b0", u"4\u00b0", u"6\u00b0"])
+    ax.set_xticks(np.linspace(0, 2*np.pi, 9, endpoint=True), labels=[r"$\phi=0$", r"", r"$\phi=\pi/2$", r"", r"$\phi=\pi$", r"", r"$\phi=3\pi/2$", r"", r""])
+    ax.set_yticks(np.linspace(0,   np.pi, 7, endpoint=True), labels=[r"$\theta=0$", r"", r"", r"$\theta=\pi/2$", r"", r"", r"$\theta=\pi$"])
 
     cm = mpl.cm.get_cmap('rainbow')
     
     scat = ax.scatter(energies[:,column2], energies[:,column1], s=60, c=energies[:,column3], vmin=emin, vmax=emax, cmap=cm)
     
-    cbar = plt.colorbar(scat, label = u"E(\u03b8, \u03d5) (meV)", pad=0.1)
+    cbar = plt.colorbar(scat, label = r"$E(\theta, \phi)\; (\mathrm{cm}^{-1})$", pad=0.15)
 
     fig.tight_layout()
     
@@ -77,5 +76,5 @@ def polar_plot(base_name = "energies", title="", columns=(0, 1, 6), factor=1.0):
 
 if __name__ == "__main__":
 
-    polar_plot(base_name = "energies_average", title="1st Ru", columns=(1,2,3), factor=1)
+    polar_plot(base_name = "energies", title="", columns=(1,2,3), factor=1, wavenumber=True)
 
